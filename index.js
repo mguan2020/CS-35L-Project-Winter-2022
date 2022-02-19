0ff7d5898a08aabcc88921720322e6362fed163b
Andres Cruz <e5ce6c2@gmail.com>
2022-02-19 01:14:02 -0800

    Added basic communication from client to server.

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const app = express();
 app.listen(3000, () => console.log('listening at 3000')); //in web browser type localhost:3000 to view webpage
 
 app.use(express.static('public')) // named public to distinguish files that will be seen by clients
+
+app.use(express.json({ limit: '5mb' })); //limits data to only 5 megabyte
+app.post('/api', (request, response) => { //post used to retrieve from client
+    console.log(request.body);
+});
