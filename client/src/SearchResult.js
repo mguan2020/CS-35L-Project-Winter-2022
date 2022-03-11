717d156f3b53bb0e795eb519052e00b6543917db
mguan2020 <mguan988@ucla.edu>
2022-03-10 21:45:29 -0800

    Fixed a minor typo with the searchresult
    Edited searchresult to say "messages in this chatroom" instead of "messages you have sent."

--- client/src/SearchResult.js
+++ client/src/SearchResult.js
@@ -8,7 +8,7 @@ function SearchResult(props) {
   const t = props.term;
   const s = props.search;
 return ( (s) ? (<div style={{backgroundColor: "skyblue"}} className="search-res">
-  <h1>Here are the messages you have sent that contain the keyword: {t} </h1>
+  <h1>Here are the messages in this chatroom that contain the keyword: {t} </h1>
          <ul style={{color: "red"}}>{listItems}</ul>
 </div>) : (<div></div>) );
   }
